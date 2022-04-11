83e6f3da35899df9ea6b7fbe549b127278d8b216
CAuSth <103417925+CAuSth@users.noreply.github.com>
2022-04-11 17:42:35 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,11 +465,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '警告处分哟';
+        if (cps <= 8) return '严重警告处分';
+        if (cps <= 10)  return '留校察看';
+        if (cps <= 15) return '直接开除';
+        return '你当校长';
     }
 
     function toStr(obj) {
